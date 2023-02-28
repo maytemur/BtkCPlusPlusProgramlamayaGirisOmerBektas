8039daee5a43b2e37de099491f97b58daefd8a6f
Mustafa Aytemur <55210284+maytemur@users.noreply.github.com>
2023-02-28 18:08:21 +0300

    Add files via upload

---
+++ Degiskenler/Degiskenler.cpp
@@ -0,0 +1,15 @@
+// Degiskenler.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    int sayi1 = 5;
+    int sayi2 = 10;
+
+    int sonuc = sayi1 + sayi2;
+
+    std::cout << sayi1 + sayi2;
+
+}
